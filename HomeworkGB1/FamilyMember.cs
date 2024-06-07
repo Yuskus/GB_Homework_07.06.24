6e5cfbb3bd0155d5e7d0a808c5cb26cb1a4fb7a5
Admin <Admin@DESKTOP-TMS7UKD>
2024-06-07 20:12:48 +0300

    Добавьте файлы проекта.

---
+++ HomeworkGB1/FamilyMember.cs
@@ -0,0 +1,80 @@
+﻿namespace HomeworkGB1
+{
+    internal class FamilyMember
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Genders Gender { get; set; }
+        public FamilyMember? Mother { get; set; }
+        public FamilyMember? Father { get; set; }
+        public List<FamilyMember>? Children { get; set; } = new();
+        public FamilyMember()
+        {
+            FullName = "Неизвестный";
+        }
+        public FamilyMember(string fullName, int age, Genders gender)
+        {
+            FullName = fullName;
+            Age = age;
+            Gender = gender;
+        }
+        public FamilyMember?[] GetGrandMothersNames()
+        {
+            return new FamilyMember?[] { Mother?.Mother, Father?.Mother };
+        }
+        public FamilyMember?[] GetGrandFathersNames()
+        {
+            return new FamilyMember?[] { Mother?.Father, Father?.Father };
+        }
+        public FamilyMember? GetHusbandOrWifeName()
+        {
+            return Gender == Genders.Male ? Children!.FirstOrDefault()?.Mother : Children!.FirstOrDefault()?.Father;
+        }
+        public FamilyMember?[] GetChildren()
+        {
+            return Children!.ToArray();
+        }
+        public FamilyMember?[] GetAllTree()
+        {
+            List<FamilyMember>? familyTree = new() { this };
+            for (int i = 0; i < familyTree.Count; i++)
+            {
+                if (familyTree[i]?.Mother != null) familyTree.Add(familyTree[i].Mother!);
+                if (familyTree[i]?.Father != null) familyTree.Add(familyTree[i].Father!);
+            }
+            return familyTree.ToArray();
+        }
+
+        public static void PrintNamesBeautifully(string enterText, FamilyMember?[] tree)
+        {
+            Console.WriteLine(enterText);
+            var beauty = new List<string>() { "" };
+            bool switcher = false;
+            for (int i = 0, div = 1; i < tree.Length; i++)
+            {
+
+                beauty[0] = beauty[0].Insert(0, "  [ " + (tree[i]?.FullName ?? "Родственник неизвестен")+ " ]  ");
+                if (switcher) { beauty[0] = beauty[0].Insert(0, "- - -"); }
+                switcher = !switcher;
+                if (i >= div - 1)
+                {
+                    beauty.Insert(0, ""); //here must be lines
+                    beauty.Insert(0, "");
+                    div = div * 2 + 1;
+                }
+            }
+            for (int i = 0; i < beauty.Count; i++)
+            {
+                Console.WriteLine(beauty[i].PadLeft((Console.WindowWidth + beauty[i].Length) / 2));
+            }
+        }
+
+        public static void PrintNames(string enterText, FamilyMember?[] members)
+        {
+            Console.WriteLine(enterText);
+            foreach (var member in members) Console.WriteLine(member?.FullName ?? "Родственник неизвестен");
+            Console.WriteLine();
+        }
+    }
+    public enum Genders { Male, Female }
+}
---
+++ HomeworkGB1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HomeworkGB1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+            var GrandPa1 = new FamilyMember("Иванов Иван Иванович", 67, Genders.Male);
+            var GrandPa2 = new FamilyMember("Петров Петр Петрович", 65, Genders.Male);;
+            var GrandMa1 = new FamilyMember("Иванова Ольга Федоровна", 65, Genders.Female);
+            var GrandMa2 = new FamilyMember("Петрова Мария Владимировна", 64, Genders.Female);
+            var Mom = new FamilyMember("Иванова Оксана Петровна", 43, Genders.Female) { Mother = GrandMa2, Father = GrandPa2 };
+            var Dad = new FamilyMember("Иванов Сергей Иванович", 45, Genders.Male) { Mother = GrandMa1, Father = GrandPa1 };
+            var Son = new FamilyMember("Иванов Алексей Сергеевич", 21, Genders.Male) { Mother = Mom, Father = Dad };
+            var Daughter = new FamilyMember("Иванова Анастасия Сергеевна", 16, Genders.Female) { Mother = Mom, Father = Dad };
+
+            GrandPa1.Children!.Add(Dad);
+            GrandMa1.Children = GrandPa1.Children;
+
+            GrandPa2.Children!.Add(Mom);
+            GrandMa2.Children = GrandPa2.Children;
+
+            Dad.Children!.Add(Son);
+            Dad.Children.Add(Daughter);
+            Mom.Children = Dad.Children;
+
+            var GrandMothersNames = Son.GetGrandMothersNames();
+            FamilyMember.PrintNames("Имена бабушек сына: ", GrandMothersNames);
+
+            var GrandFathersNames = Daughter.GetGrandFathersNames();
+            FamilyMember.PrintNames("Имена дедушек дочери: ", GrandFathersNames);
+
+            Console.WriteLine("Муж мамы: " + Mom.GetHusbandOrWifeName()?.FullName + "\n");
+
+            Console.WriteLine("Жена отца: " + Dad.GetHusbandOrWifeName()?.FullName + "\n");
+
+            FamilyMember.PrintNames("Имена детей матери: ", Mom.GetChildren());
+
+            FamilyMember.PrintNamesBeautifully("Древо: ", Son.GetAllTree());
+        }
+    }
+}
